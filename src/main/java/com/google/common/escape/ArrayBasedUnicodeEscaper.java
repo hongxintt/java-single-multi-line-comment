272b350440c6b244cdbd6b0280cb49363b901eb4
jursjohn <johnjurassic93@yahoo.com.ph>
2022-06-04 03:31:10 +0000

    Update at 2022-06-04T03:31:10.123887Z

--- src/main/java/com/google/common/escape/ArrayBasedUnicodeEscaper.java
+++ src/main/java/com/google/common/escape/ArrayBasedUnicodeEscaper.java
@@ -22,7 +22,7 @@ public abstract class ArrayBasedUnicodeEscaper {
          below the values of characters in surrogate pairs, this cannot occur.
          This approach does mean that we break out of the fast path code in cases
          where we don't strictly need to, but this situation will almost never
-        */ occur in practice.
+         occur in practice.*/
         if (safeMin >= Character.MIN_HIGH_SURROGATE) {
             this.safeMinChar = Character.MAX_VALUE;
             this.safeMaxChar = 0;
@@ -32,15 +32,13 @@ public abstract class ArrayBasedUnicodeEscaper {
         }
     }
 
-    //
-    // This is overridden to improve performance. Rough benchmarking shows that this almost doubles
-    // the speed when processing strings that do not require any escaping.
-     //
+//     * This is overridden to improve performance. Rough benchmarking shows that this almost doubles
+//     * the speed when processing strings that do not require any escaping.
     public final String escape(String s) {
         return s;
     }
 
-    // Overridden for performance.
+//     Overridden for performance.
     protected final int nextEscapeIndex(CharSequence csq, int index, int end) {
         return index;
     }
