5aeead52d3fb353d6d1ef54ca61d0d561164a3a7
SirWinstilkinsonQuindalimous <nicholas.schaffer2018@gmail.com>
2022-07-05 21:30:56 +0000

    Update at 2022-07-05T21:30:56.525553Z

--- src/main/java/com/google/common/escape/ArrayBasedUnicodeEscaper.java
+++ src/main/java/com/google/common/escape/ArrayBasedUnicodeEscaper.java
@@ -10,7 +10,8 @@ public abstract class ArrayBasedUnicodeEscaper {
     private char safeMaxChar;
 
     protected ArrayBasedUnicodeEscaper() {
-        /* This is a bit of a hack but lets us do quicker per-character checks in
+        /* 
+         This is a bit of a hack but lets us do quicker per-character checks in
          the fast path code. The safe min/max values are very unlikely to extend
          into the range of surrogate characters, but if they do we must not test
          any values in that range. To see why, consider the case where:
@@ -22,7 +23,8 @@ public abstract class ArrayBasedUnicodeEscaper {
          below the values of characters in surrogate pairs, this cannot occur.
          This approach does mean that we break out of the fast path code in cases
          where we don't strictly need to, but this situation will almost never
-         occur in practice. */
+         occur in practice. 
+         */
         if (safeMin >= Character.MIN_HIGH_SURROGATE) {
             this.safeMinChar = Character.MAX_VALUE;
             this.safeMaxChar = 0;
@@ -33,8 +35,8 @@ public abstract class ArrayBasedUnicodeEscaper {
     }
 
     /*
-     * This is overridden to improve performance. Rough benchmarking shows that this almost doubles
-     * the speed when processing strings that do not require any escaping.
+     This is overridden to improve performance. Rough benchmarking shows that this almost doubles
+     the speed when processing strings that do not require any escaping.
      */
     public final String escape(String s) {
         return s;
